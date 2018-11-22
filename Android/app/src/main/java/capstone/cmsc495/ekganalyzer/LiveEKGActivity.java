f2105176be6294c61a5500a236dc79031be0a7b5
Jon Simmons <zenwarrior01@gmail.com>
2018-11-21 22:22:13 -0800

    fixed menu nav, added live ekg activity

--- Android/app/src/main/java/capstone/cmsc495/ekganalyzer/LiveEKGActivity.java
+++ Android/app/src/main/java/capstone/cmsc495/ekganalyzer/LiveEKGActivity.java
@@ -1,8 +1,9 @@
 package capstone.cmsc495.ekganalyzer;
+
 /**
- * @Purpose this activity holds the EKG history
+ * @Purpose this activity holds the Live EKG
  * @author: Deo & Jon Simmons
- * @since 11-18-2018
+ * @since 11-21-2018
  * @version 1
  */
 
@@ -17,16 +18,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
 import android.view.MenuItem;
 
-public class HistoryActivity extends AppCompatActivity {
+public class LiveEKGActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         // Inflate the view
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_history);
+        setContentView(R.layout.activity_live_ekg);
 
         // Set up a custom tool bar
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -43,7 +43,7 @@ public class HistoryActivity extends AppCompatActivity {
 
         // Set the drawer layout A.K.A the navigation bar
         drawerLayout = findViewById(R.id.drawer_layout);
-        final HistoryActivity thisActivity = this;
+        final LiveEKGActivity thisActivity = this;
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
@@ -59,11 +59,10 @@ public class HistoryActivity extends AppCompatActivity {
                         intent = new Intent(thisActivity, RhythmsActivity.class);
                         startActivity(intent);
                         return true;
-                    case R.id.live_ekg:
-                        intent = new Intent(thisActivity, LiveEKGActivity.class);
+                    case R.id.historyList:
+                        intent = new Intent(thisActivity, HistoryActivity.class);
                         startActivity(intent);
                         return true;
-
                     case R.id.log_out:
                         // ##### TO DO: Log user out
 
@@ -79,7 +78,8 @@ public class HistoryActivity extends AppCompatActivity {
                 }// End switch statement
             }// End onNavigationItemSelected
         });// End closure
-    }// End onCreate() Method
+    } // onCreate()
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -89,8 +89,8 @@ public class HistoryActivity extends AppCompatActivity {
             case android.R.id.home:
                 drawerLayout.openDrawer(Gravity.START);
                 return true;
-            case R.id.live_ekg:
-                intent = new Intent(this, LiveEKGActivity.class);
+            case R.id.historyList:
+                intent = new Intent(this, HistoryActivity.class);
                 startActivity(intent);
                 return true;
             case R.id.rhythms:
@@ -103,5 +103,4 @@ public class HistoryActivity extends AppCompatActivity {
         }// End switch statement
 
     }// End onOptionsItemSelected() Method
-
-}// End History class
+} // LiveEKGActivity class
--- Android/app/src/main/java/capstone/cmsc495/ekganalyzer/RhythmsActivity.java
+++ Android/app/src/main/java/capstone/cmsc495/ekganalyzer/RhythmsActivity.java
@@ -1,12 +1,20 @@
 package capstone.cmsc495.ekganalyzer;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class RhythmsActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +24,52 @@ public class RhythmsActivity extends AppCompatActivity {
         // Set up a custom tool bar
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Get the action bar with an item
+        ActionBar actionBar = getSupportActionBar();
+        System.out.println(actionBar);
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        }// End if
+
+
+        // Set the drawer layout A.K.A the navigation bar
+        drawerLayout = findViewById(R.id.drawer_layout);
+        final RhythmsActivity thisActivity = this;
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                menuItem.setChecked(true);
+                drawerLayout.closeDrawers();
+
+                switch (menuItem.getItemId()) {
+                    case R.id.historyList:
+                        Intent intent = new Intent(thisActivity, HistoryActivity.class);
+                        startActivity(intent);
+                        return true;
+                    case R.id.live_ekg:
+                        intent = new Intent(thisActivity, LiveEKGActivity.class);
+                        startActivity(intent);
+                        return true;
+                    case R.id.log_out:
+                        // ##### TO DO: Log user out
+
+
+
+
+                        intent = new Intent(thisActivity, LoginActivity.class);
+                        startActivity(intent);
+                        return true;
+                    default:
+                        return true;
+
+                }// End switch statement
+            }// End onNavigationItemSelected
+        });// End closure
 
         if (getSupportActionBar() != null)
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -36,7 +90,23 @@ public class RhythmsActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Menu selection options will go here
-        return super.onOptionsItemSelected(item);
+        Intent intent;
+
+        switch (item.getItemId()){
+            case android.R.id.home:
+                drawerLayout.openDrawer(Gravity.START);
+                return true;
+            case R.id.historyList:
+                intent = new Intent(this, HistoryActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.live_ekg:
+                intent = new Intent(this, LiveEKGActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }// End switch statement
     }// End onOptionsItemsSelected() Method
 }// End RhythmsActivity class
