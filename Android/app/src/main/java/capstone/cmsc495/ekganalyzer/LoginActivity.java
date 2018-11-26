4eea6d6361ca2eb9371b08264ce70d511c2439a3
Deo Kalule <deograstius@gmail.com>
2018-11-25 23:20:16 -0500

    Login Error

--- Android/app/src/main/java/capstone/cmsc495/ekganalyzer/LoginActivity.java
+++ Android/app/src/main/java/capstone/cmsc495/ekganalyzer/LoginActivity.java
@@ -8,17 +8,39 @@ package capstone.cmsc495.ekganalyzer;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.amazonaws.amplify.generated.graphql.GetUserQuery;
 import com.amazonaws.mobile.config.AWSConfiguration;
 import com.amazonaws.mobileconnectors.appsync.AWSAppSyncClient;
+import com.amazonaws.mobileconnectors.appsync.fetcher.AppSyncResponseFetchers;
+import com.apollographql.apollo.GraphQLCall;
+import com.apollographql.apollo.api.Response;
+import com.apollographql.apollo.exception.ApolloException;
+
+import javax.annotation.Nonnull;
 
 public class LoginActivity extends AppCompatActivity {
+
+    AWSAppSyncClient appSyncClient;
+    EditText passwordText;
+    final String TAG = "LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        passwordText = findViewById(R.id.passwordTextField);
+
+        appSyncClient = AWSAppSyncClient.builder()
+                .context(getApplicationContext())
+                .awsConfiguration(new AWSConfiguration(getApplicationContext()))
+                .build();
+
     }// End onCreate() Method
 
     /**
@@ -35,7 +57,30 @@ public class LoginActivity extends AppCompatActivity {
      * @param view the button that was tapped
      */
     public void goToHistoryActivity(View view) {
-        Intent intent = new Intent(this, HistoryActivity.class);
+
+        Intent intent = new Intent(LoginActivity.this, HistoryActivity.class);
         startActivity(intent);
+
+//        appSyncClient.query(GetUserQuery.builder().id(passwordText.getText().toString()).build())
+//                .responseFetcher(AppSyncResponseFetchers.CACHE_AND_NETWORK)
+//                .enqueue(new GraphQLCall.Callback<GetUserQuery.Data>() {
+//                    @Override
+//                    public void onResponse(@Nonnull Response<GetUserQuery.Data> response) {
+//                        if(response.data() != null){
+//                            Log.v(TAG, "Not NIl");
+//                            Log.v(TAG, response.data().getUser().password());
+////
+//                        } else {
+//                            Log.v(TAG, "response.data() is Null");
+//                        }
+//                    }
+//
+//                    @Override
+//                    public void onFailure(@Nonnull ApolloException e) {
+//                        Toast.makeText(LoginActivity.this, e.getCause().toString(), Toast.LENGTH_SHORT).show();
+//                    }
+//                });
+
+
     }// End goToHistoryActivity() class
 }// End LoginActivity class
--- Android/app/src/main/java/capstone/cmsc495/ekganalyzer/SignUpActivity.java
+++ Android/app/src/main/java/capstone/cmsc495/ekganalyzer/SignUpActivity.java
@@ -1,20 +1,16 @@
 package capstone.cmsc495.ekganalyzer;
 
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.amazonaws.amplify.generated.graphql.CreateUserMutation;
 import com.amazonaws.mobile.config.AWSConfiguration;
 import com.amazonaws.mobileconnectors.appsync.AWSAppSyncClient;
-import com.amazonaws.mobileconnectors.appsync.sigv4.BasicCognitoUserPoolsAuthProvider;
-import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserPool;
 import com.apollographql.apollo.GraphQLCall;
 import com.apollographql.apollo.api.Response;
 import com.apollographql.apollo.exception.ApolloException;
@@ -26,8 +22,6 @@ import type.CreateUserInput;
 public class SignUpActivity extends AppCompatActivity {
 
     AWSAppSyncClient appSyncClient;
-    CognitoUserPool cognitoUserPool;
-    BasicCognitoUserPoolsAuthProvider cognitoUserPoolsAuthProvider;
     EditText emailEditText;
     EditText passwordEditText;
     EditText sensorIDEditText;
@@ -39,14 +33,11 @@ public class SignUpActivity extends AppCompatActivity {
 
         Context context = getApplicationContext();
         AWSConfiguration awsConfiguration = new AWSConfiguration(context);
-        cognitoUserPool = new CognitoUserPool(context, awsConfiguration);
-        cognitoUserPoolsAuthProvider = new BasicCognitoUserPoolsAuthProvider(cognitoUserPool);
 
 
         appSyncClient = AWSAppSyncClient.builder()
                 .context(context)
                 .awsConfiguration(awsConfiguration)
-                .cognitoUserPoolsAuthProvider(cognitoUserPoolsAuthProvider)
                 .build();
 
         emailEditText = findViewById(R.id.emailTextField);
@@ -58,35 +49,36 @@ public class SignUpActivity extends AppCompatActivity {
         return emailEditText.getText().toString().equals("") ||
                 passwordEditText.getText().toString().equals("") ||
                 sensorIDEditText.getText().toString().equals("");
-    }// End
+    }// End textFieldsAreEmpty() Method
+
+
 
     private GraphQLCall.Callback<CreateUserMutation.Data> mutationCallBack = new GraphQLCall.Callback<CreateUserMutation.Data>() {
         @Override
         public void onResponse(@Nonnull Response<CreateUserMutation.Data> response) {
-            Log.v("SignUpAcitvity", response.toString());
+            Log.v("SignUpActivity", response.toString());
             finish();
         }
 
         @Override
         public void onFailure(@Nonnull ApolloException e) {
-            Log.v("SignUpAcitvity", e.getCause().toString());
+            Log.v("SignUpActivity", e.getCause().toString());
             e.printStackTrace();
         }
     };// End property
 
     public void signUp(View view) {
 
-        if(!textFieldsAreEmpty()){
+        if (!textFieldsAreEmpty()) {
             CreateUserInput createUserInput = CreateUserInput.builder()
                     .email(emailEditText.getText().toString())
                     .password(passwordEditText.getText().toString())
                     .sensorID(sensorIDEditText.getText().toString())
                     .build();
 
-            appSyncClient.mutate(CreateUserMutation.builder().input(createUserInput).build())
-                    .enqueue(mutationCallBack);
-
-
+            appSyncClient.mutate(
+                    CreateUserMutation.builder().input(createUserInput).build()
+            ).enqueue(mutationCallBack);
         } else {
             Toast.makeText(this, "Please fill out all 3 fields", Toast.LENGTH_SHORT).show();
         }
